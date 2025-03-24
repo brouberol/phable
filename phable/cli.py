273bd7378a781913b4db8d98105a727dbc750f0c
Balthazar Rouberol <br@imap.cc>
2025-03-24 10:10:54 +0100

    Display task projects/tags
    Signed-off-by: Balthazar Rouberol <br@imap.cc>

--- phable/cli.py
+++ phable/cli.py
@@ -53,10 +53,24 @@ class PhabricatorClient:
         except requests.RequestException as e:
             raise Exception(f"API request failed: {str(e)}")
 
+    def create_task(self, fields: dict[str, Any]) -> dict[str, Any]:
+        """Create a new Maniphest task"""
+        return self._make_request("maniphest.create", params={"fields": fields})
+
+    def edit_task(self, fields: dict[str, Any]) -> dict[str, Any]:
+        """Edit a Maniphest task"""
+        return self._make_request("maniphest.edit", params={"fields": fields})
+
     def show_task(self, task_id: int) -> dict[str, Any]:
         """Show a Maniphest task"""
         return self._make_request(
-            "maniphest.search", params={"constraints[ids][0]": task_id}
+            "maniphest.search",
+            params={
+                "constraints[ids][0]": task_id,
+                "attachments[subscribers]": "true",
+                "attachments[projects]": "true",
+                "attachments[columns]": "true",
+            },
         )["result"]["data"][0]
 
     @cache
@@ -65,6 +79,14 @@ class PhabricatorClient:
         return self._make_request(
             "user.search", params={"constraints[phids][0]": phid}
         )["result"]["data"][0]
+
+    def show_projects(self, phids: list[str]) -> dict[str, Any]:
+        """Show a Maniphest project"""
+        params = {}
+        for i, phid in enumerate(phids):
+            params[f"constraints[phids][{i}]"] = phid
+        return self._make_request("project.search", params=params)["result"]["data"]
+
 
 
 @cli.command()
@@ -75,10 +97,20 @@ def show(task_id: int):
     if task := client.show_task(task_id):
         author = client.show_user(phid=task["fields"]["authorPHID"])
         owner = client.show_user(phid=task["fields"]["ownerPHID"])
+        if project_ids := task["attachments"]["projects"]["projectPHIDs"]:
+            tags = [
+                f"{project['fields']['parent']['name']} - {project['fields']['name']}"
+                if project["fields"]["parent"]
+                else project["fields"]["name"]
+                for project in client.show_projects(phids=project_ids)
+            ]
+        else:
+            tags = []
         click.echo(f"Task: T{task_id}")
         click.echo(f"Title: {task['fields']['name']}")
         click.echo(f"Author: {author['fields']['username']}")
         click.echo(f"Owner: {owner['fields']['username']}")
+        click.echo(f"Tags: {', '.join(tags)}")
         click.echo(f"Status: {task['fields']['status']['name']}")
         click.echo(f"Priority: {task['fields']['priority']['name']}")
         click.echo(f"Description: {task['fields']['description']['raw']}")
