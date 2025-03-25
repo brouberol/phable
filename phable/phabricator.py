08c87dd896d11f665b3f2e0f9bf5d43feb78508f
Balthazar Rouberol <br@imap.cc>
2025-03-25 08:25:25 +0100

    Improve docstrings
    Signed-off-by: Balthazar Rouberol <br@imap.cc>

--- phable/phabricator.py
+++ phable/phabricator.py
@@ -63,6 +63,7 @@ class PhabricatorClient:
     def create_or_edit_task(
         self, params: dict[str, Any], task_id: int | None = None
     ) -> dict[str, Any]:
+        """Create or edit (if a task_id is provided) a Maniphest task."""
         raw_params = {}
         for i, (key, value) in enumerate(params.items()):
             raw_params[f"transactions[{i}][type]"] = key
@@ -88,11 +89,13 @@ class PhabricatorClient:
         )["result"]["data"][0]
 
     def find_tasks_with_parent(self, parent_id: int) -> list[dict[str, Any]]:
+        """Return details of all Maniphest subtasks of the provided task id"""
         return self._make_request(
             "maniphest.search", params={"constraints[parentIDs][0]": parent_id}
         )["result"]["data"]
 
     def find_subtask_parent(self, subtask_id: int) -> dict[str, Any] | None:
+        """Return details of the parent Maniphest task for the provided task id"""
         return self._first(
             self._make_request(
                 "maniphest.search", params={"constraints[subtaskIDs][0]": subtask_id}
@@ -100,47 +103,58 @@ class PhabricatorClient:
         )
 
     def move_task_to_column(self, task_id: int, column_phid: str) -> dict[str, Any]:
+        """Move the argument task to column of associated column id"""
         return self.create_or_edit_task(task_id=task_id, params={"column": column_phid})
 
     def mark_task_as_resolved(self, task_id: int) -> dict[str, Any]:
+        """Set the status of the argument task to Resolved"""
         return self.create_or_edit_task(task_id=task_id, params={"status": "Resolved"})
 
     @cache
     def show_user(self, phid: str) -> dict[str, Any] | None:
-        """Show a Maniphest user"""
+        """Show details of a Maniphest user"""
         user = self._make_request(
             "user.search", params={"constraints[phids][0]": phid}
         )["result"]["data"]
         return self._first(user)
 
     def show_projects(self, phids: list[str]) -> dict[str, Any]:
-        """Show a Maniphest project"""
+        """Show details of the provided Maniphest projects"""
         params = {}
         for i, phid in enumerate(phids):
             params[f"constraints[phids][{i}]"] = phid
         return self._make_request("project.search", params=params)["result"]["data"]
 
     def current_user(self) -> dict[str, Any]:
+        """Return details of the user associated with the phabricator API token"""
         return self._make_request("user.whoami")["result"]
 
     def find_user_by_username(self, username: str) -> dict[str, Any] | None:
+        """Return user details of the user with the provided username"""
         user = self._make_request(
             "user.search", params={"constraints[usernames][0]": username}
         )["result"]["data"]
         return self._first(user)
 
     def assign_task_to_user(self, task_id: int, user_phid: int) -> dict[str, Any]:
+        """Set the owner of the argument task to the argument user id"""
         return self.create_or_edit_task(task_id=task_id, params={"owner": user_phid})
 
     def list_project_columns(
         self,
         project_phid: str,
     ) -> list[dict[str, Any]]:
+        """Return the details of each column in a given project"""
         return self._make_request(
             "project.column.search", params={"constraints[projects][0]": project_phid}
         )["result"]["data"]
 
     def get_project_current_milestone(self, project_phid: str) -> dict[str, Any] | None:
+        """Return the first non hidden column associated with a subproject.
+
+        We assume it to be associated with the current milestone.
+
+        """
         columns = self.list_project_columns(project_phid)
         for column in columns:
             if column["fields"]["proxyPHID"] and not column["fields"]["isHidden"]:
