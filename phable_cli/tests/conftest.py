cb9e0caccb5898bece8adbbb3aecb1e229534e6d
Balthazar Rouberol <br@imap.cc>
2025-04-11 20:08:13 +0200

    Fix tests
    Signed-off-by: Balthazar Rouberol <br@imap.cc>

--- phable_cli/tests/conftest.py
+++ phable_cli/tests/conftest.py
@@ -1,8 +1,9 @@
 import json
+from pathlib import Path
 
 import pytest
 
 
 @pytest.fixture
 def simple_task_response():
-    return json.load(open('fixtures/simple_task.json'))
+    return json.load(open(Path(__file__).parent / Path("fixtures/simple_task.json")))
