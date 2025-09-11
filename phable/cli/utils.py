5843a7ca49290403ce63b69383d44899e6e071f1
Guillaume Lederrey <guillaume.lederrey@gmail.com>
2025-09-11 10:16:19 +0200

    feat(move): Project-PHID as a CLI argument for the move command
    When working with multiple projects, it is cumbersome to edit
    config file or environment variables each time a different
    project is needed. This commit introduces a "--project-phid" argument
    to the "move" command, which would override
    "phabricator_default_project_phid".
    
    It might make sense in a next step to introduce the same
    "--project-phid" argument to other commands where it makes sense.

--- phable/cli/utils.py
+++ phable/cli/utils.py
@@ -1,1 +1,14 @@
+import click
+
 VARIADIC = -1
+
+project_phid_option = click.option(
+    "--project-phid",
+    default=None,
+    required=False,
+    help=(
+            "The command will operate on the given project (tag), or to the active "
+            "milestone of this project if --milestone is given. If no project "
+            "is given, the default project in configuration file is used instead."
+    ),
+)
